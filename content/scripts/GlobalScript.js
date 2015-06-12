80597ef0f5de84c2c09da618f4650480a2d58c86
Reizon Granado <xreizonx@gmail.com>
2015-06-12 14:59:37 -0430

    nada

--- content/scripts/GlobalScript.js
+++ content/scripts/GlobalScript.js
@@ -1,26 +1,24 @@
 
 $(document).ready(function(){
-		
-		$('#form').submit(function(event){
-			event.preventDefault();
-			var url = $(this).attr('action');
-			var dataVa = $(this).serialize();
-			$.ajax({
-				url: url,
-				type: 'POST',
-				data: dataVa,
-				success: function (mensaje) {
-					$(".resultado").html(mensaje);
-					var vericidad = $('#notificacion').attr('class');
-					
-					if(vericidad == "success"){
-						setTimeout(function(){
-							window.location = "index.php";
-						},1500);
-					}
-					
+	$('#form').submit(function(event){
+		event.preventDefault();
+		var url = $(this).attr('action');
+		var dataVa = $(this).serialize();
+		$.ajax({
+			url: url,
+			type: 'POST',
+			data: dataVa,
+			success: function (mensaje) {
+				$(".resultado").html(mensaje);
+				var vericidad = $('#notificacion').attr('class');
+				
+				if(vericidad == "success"){
+					setTimeout(function(){
+						window.location = "index.php";
+					},1500);
 				}
-			});
-			return false;
+			}
 		});
+		return false;
+	});
 });
